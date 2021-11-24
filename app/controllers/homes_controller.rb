185dd0c12211758195aae6c5c8efdd3ac6408603
usho1024 <usho1024@gmail.com>
2021-11-24 10:19:27 +0000

    [Add] タイムライン機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,9 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
 
   def top
-    @tweets = Tweet.all
+    # whereメソッドでログインユーザーとそのフォローしているユーザーのつぶやきをタイムラインに表示する
+    # "*"は可変長引数で、複数の引数（フォローしているユーザーID）を設定することができる
+    @tweets = Tweet.where(user_id: [current_user.id, *current_user.following_ids])
     @current_user = current_user
     @tweet_new = Tweet.new
   end
